46fd57bea882aaac1cc957b3f9eb6bf8b2b0d203
Krisztian Nagy <k.nagy.k98@gmail.com>
2020-10-31 16:16:18 +0000

    Updates

---
+++ precache-manifest.6f8477cfc56b3f84f7b22858380f2f85.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3278a3b7acef4191c705b57ab4ab5d00",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "050072e68b66ad28d57a",
+    "url": "/portfolio/static/css/main.c5f058cf.chunk.css"
+  },
+  {
+    "revision": "179ef49c524d8fe0e0f3",
+    "url": "/portfolio/static/js/2.2623bd20.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/portfolio/static/js/2.2623bd20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050072e68b66ad28d57a",
+    "url": "/portfolio/static/js/main.bdd3a184.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "71c26df6450d68f85b23db5cf8ab3d98",
+    "url": "/portfolio/static/media/Ecommerce_1.71c26df6.png"
+  },
+  {
+    "revision": "be6dbef8ac5e3c9929168f83aa9cb3c9",
+    "url": "/portfolio/static/media/Facebook_1.be6dbef8.png"
+  },
+  {
+    "revision": "366225c6d25bcdc3ac15e8a31898d710",
+    "url": "/portfolio/static/media/Interface_Design_Background.366225c6.svg"
+  },
+  {
+    "revision": "a189dad8fb2a41e10a502b5b9b651d61",
+    "url": "/portfolio/static/media/Interface_Design_Icon.a189dad8.svg"
+  },
+  {
+    "revision": "e2910d59ec1978c1e4b3dda19288a06b",
+    "url": "/portfolio/static/media/Linda_1.e2910d59.png"
+  },
+  {
+    "revision": "667c2ef33e73500942cddcc3736b2c85",
+    "url": "/portfolio/static/media/Motion_Graphic_Background.667c2ef3.svg"
+  },
+  {
+    "revision": "8616744e85a055db42f891202eaffbbc",
+    "url": "/portfolio/static/media/Motion_Graphic_Icon.8616744e.svg"
+  },
+  {
+    "revision": "02adc586ddd5449cc2386287d0205757",
+    "url": "/portfolio/static/media/Netflix_1.02adc586.png"
+  },
+  {
+    "revision": "f2dc1781d8179f64dd20763e042eb208",
+    "url": "/portfolio/static/media/Portfolio_1.f2dc1781.png"
+  },
+  {
+    "revision": "244a6943a6c4f9cff2c0cb66963268d9",
+    "url": "/portfolio/static/media/Seo_Optimize_Background.244a6943.svg"
+  },
+  {
+    "revision": "e0d2d61c91706a49591fd927d926df13",
+    "url": "/portfolio/static/media/Seo_Optimize_Icon.e0d2d61c.svg"
+  },
+  {
+    "revision": "54cfaf807f0acf18a443827c824b8dc2",
+    "url": "/portfolio/static/media/ShapeDark.54cfaf80.svg"
+  },
+  {
+    "revision": "7a06adffa18a4c51b79517ecd272be41",
+    "url": "/portfolio/static/media/ShapeLight.7a06adff.svg"
+  },
+  {
+    "revision": "292c9f85f52841f73dd21af0db0bf20b",
+    "url": "/portfolio/static/media/UX_Design_Background.292c9f85.svg"
+  },
+  {
+    "revision": "3b92ac9d80da4d454acf34101f2dd378",
+    "url": "/portfolio/static/media/UX_Design_Icon.3b92ac9d.svg"
+  },
+  {
+    "revision": "778c93c10deaa087d192721bd4d2f314",
+    "url": "/portfolio/static/media/Web_Design_Background.778c93c1.svg"
+  },
+  {
+    "revision": "ef23c0a842081920a081304da1c8d332",
+    "url": "/portfolio/static/media/Web_Design_Icon.ef23c0a8.svg"
+  },
+  {
+    "revision": "5ee93d372c8f2dcb6384d8364f921334",
+    "url": "/portfolio/static/media/Web_Development_Background.5ee93d37.svg"
+  },
+  {
+    "revision": "3cf6c7c718aa25d122f1c2511e3b7eb5",
+    "url": "/portfolio/static/media/Web_Development_Icon.3cf6c7c7.svg"
+  },
+  {
+    "revision": "57c961afd6c471e652d5ade4471b009e",
+    "url": "/portfolio/static/media/bg_flag.57c961af.svg"
+  },
+  {
+    "revision": "593559e068fd3ddfb277e3ad8cc3ce1a",
+    "url": "/portfolio/static/media/fb_logo.593559e0.svg"
+  },
+  {
+    "revision": "f14b4ee6edeaafa0a247b0eb2943469a",
+    "url": "/portfolio/static/media/github_logo.f14b4ee6.svg"
+  },
+  {
+    "revision": "2965b9ca68c5c02c3fb0ba99e8a15a6f",
+    "url": "/portfolio/static/media/linkedin_logo.2965b9ca.svg"
+  },
+  {
+    "revision": "b528844af0aa17aacb3c5e00ce5189f6",
+    "url": "/portfolio/static/media/me.b528844a.jpg"
+  },
+  {
+    "revision": "bb946e7cb15206612ae7f3b5f83acd19",
+    "url": "/portfolio/static/media/me.bb946e7c.jpg"
+  }
+]);
